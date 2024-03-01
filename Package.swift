b6d89e7b9d364f6ae82f83453be10d093441cb10
Ash Gandhi <ash@storyboard.fm>
2024-03-01 15:41:30 -0500

    Test

--- Package.swift
+++ Package.swift
@@ -8,11 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "PvPorcupine",
-            targets: ["PvPorcupine"]),
-        .library(
             name: "Porcupine",
-            targets: ["Porcupine"]),
+            targets: ["PvPorcupine", "Porcupine"]),
     ],
     dependencies: [
     ],
